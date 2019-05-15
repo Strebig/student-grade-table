10c225074c3be6f956a8a8ce1574c319aeda0ce2
Strebig <strebig.nick@gmail.com>
2019-05-15 23:33:24 +0000

    Added Fetch Method for All Grades on Server - Updating State

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -6,12 +6,26 @@ class App extends React.Component {
     this.state = {
       grades: []
     };
+    this.getAllGrades = this.getAllGrades.bind(this);
   }
 
   header() {
     let header = 'Student Grade Table';
     return header;
   }
+
+  componentDidMount() {
+    this.getAllGrades();
+  }
+
+  getAllGrades() {
+    fetch('/api/grades')
+      .then(grades => grades.json())
+      .then(grades => {
+        this.setState({ grades: grades });
+      });
+  }
+
   render() {
     return (
       <h1>{this.header()}</h1>
